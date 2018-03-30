694b460e25f452699490888e739b34d4e6004dd6
Christian Talmo <talmo.christian@gmail.com>
2018-03-30 14:33:00 +0300

    fix prefix + I ( capital I )

--- install-tpm.sh
+++ install-tpm.sh
@@ -1,4 +1,4 @@
 # Install tpm plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # then press "prefix + I" to install tmux plugins
-echo'"then press "prefix + i" to install tmux plugins'
+echo'"then press "prefix + I" to install tmux plugins'
