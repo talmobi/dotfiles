b71f1ce621ce9cadadd2428dea914fa40fa20102
Christian Talmo <talmo.christian@gmail.com>
2018-03-30 20:07:10 +0300

    fixed tmux clipboard

--- install-tpm.sh
+++ install-tpm.sh
@@ -2,3 +2,6 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # then press "prefix + I" to install tmux plugins
 echo'"then press "prefix + I" to install tmux plugins'
+
+# for clibboard to work probaly need to install this also
+# brew install reattach-to-user-namespace
