001b3dcef556682f5ba732700d349e95b992213f
talmobi <talmo.christian@gmail.com>
2020-09-22 20:56:02 +0200

    prompt to delete before copying

--- link.sh
+++ link.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 cd ~ # goto home directory
+
+rm -i .bashrc
+rm -i .vimrc
+rm -i .vim
+rm -i .tmux
+rm -i .tmux.conf
+
 ln -s ~/dotfiles/.bashrc .
 ln -s ~/dotfiles/.vimrc .
 ln -s ~/dotfiles/.vim .
