c2ac5b0afb0a6475932976bebdbf89f101b57d73
Christian Talmo <talmo.christian@gmail.com>
2021-05-26 22:06:11 +0300

    use .bash_profile to load both .profile and .bashrc for login shells

--- link.sh
+++ link.sh
@@ -3,12 +3,14 @@
 cd ~ # goto home directory
 
 rm -i .bashrc
+rm -i .bash_profile
 rm -i .vimrc
 rm -i .vim
 rm -i .tmux
 rm -i .tmux.conf
 
 ln -s ~/dotfiles/.bashrc .
+ln -s ~/dotfiles/.bash_profile .
 ln -s ~/dotfiles/.vimrc .
 ln -s ~/dotfiles/.vim .
 ln -s ~/dotfiles/.tmux .
