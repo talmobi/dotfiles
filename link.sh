69d2d7bff158b1596aec2d61ae668488c2ab754b
Christian Talmo <talmo.christian@gmail.com>
2021-05-27 14:09:06 +0300

    link .ctags

--- link.sh
+++ link.sh
@@ -8,6 +8,7 @@ rm -i .vimrc
 rm -i .vim
 rm -i .tmux
 rm -i .tmux.conf
+rm -i .ctags
 
 ln -s ~/dotfiles/.bashrc .
 ln -s ~/dotfiles/.bash_profile .
@@ -15,3 +16,4 @@ ln -s ~/dotfiles/.vimrc .
 ln -s ~/dotfiles/.vim .
 ln -s ~/dotfiles/.tmux .
 ln -s ~/dotfiles/.tmux.conf .
+ln -s ~/dotfiles/.ctags .
